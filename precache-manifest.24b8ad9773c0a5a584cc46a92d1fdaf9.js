6004ce9070ffeb045378eb1b95738634e2fbc4f0
Josh <joshualazarte@yahoo.com>
2019-03-05 21:12:58 -0600

    Updates

---
+++ precache-manifest.24b8ad9773c0a5a584cc46a92d1fdaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "8a45fcb2db7edd8d181f",
+    "url": "/contact-manager/static/js/main.8a45fcb2.chunk.js"
+  },
+  {
+    "revision": "8a50a3560e27760b908d",
+    "url": "/contact-manager/static/js/2.8a50a356.chunk.js"
+  },
+  {
+    "revision": "8a45fcb2db7edd8d181f",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "8a50a3560e27760b908d",
+    "url": "/contact-manager/static/css/2.efb9a0fc.chunk.css"
+  },
+  {
+    "revision": "d965e133f9a6d14aa0f01a504cc1ddac",
+    "url": "/contact-manager/index.html"
+  }
+];
